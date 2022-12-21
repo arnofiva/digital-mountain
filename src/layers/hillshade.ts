19198150a2850f72869951ad565440dc9a1995c0
Arno Fiva <arnofiva@gmail.com>
2022-12-21 12:40:07 +0100

    Always show hillshade

--- src/layers/hillshade.ts
+++ src/layers/hillshade.ts
@@ -12,16 +12,17 @@ const color = new Color("#3A4754");
 const hillshade = new TileLayer({
   portalItem: { id: "1b243539f4514b6ba35e7d995890db1d" }, // Light
   // url: "https://services.arcgisonline.com/arcgis/rest/services/Elevation/World_Hillshade/MapServer",
-  blendMode: "luminosity",
-  visible: false,
+  blendMode: "multiply",
+  visible: true,
+  opacity: 0.5,
   title: "World Hillshade (Blended)"
 });
 
 hillshade.load().then(() => {
   // console.log({ hillshade });
 
-  const lods = hillshade.tileInfo.lods.slice(0, 15);
-  hillshade.tileInfo.lods = lods;
+  // const lods = hillshade.tileInfo.lods.slice(0, 15);
+  // hillshade.tileInfo.lods = lods;
 });
 
 export default hillshade;
