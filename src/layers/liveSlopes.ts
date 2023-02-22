2080d28e688614a5392fdde552761efa5a04c3d9
Arno Fiva <arnofiva@gmail.com>
2023-02-22 16:10:15 +0100

    Styling of live data

--- src/layers/liveSlopes.ts
+++ src/layers/liveSlopes.ts
@@ -1,36 +1,61 @@
 import StreamLayer from "@arcgis/core/layers/StreamLayer";
 import { UniqueValueRenderer } from "@arcgis/core/renderers";
+import { FillSymbol3DLayer, PolygonSymbol3D } from "@arcgis/core/symbols";
 
-import { defaultSlopeSymbol, openSlopeSymbol, preparingSlopeSymbol } from "../symbols";
 
 const url =
   "https://us-iot.arcgis.com/bc1qjuyagnrebxvh/bc1qjuyagnrebxvh/streams/arcgis/rest/services/slopesStatusStream/StreamServer";
 
-const createSlopeStream = () =>
-  new StreamLayer({
-    url,
-    title: "Slopes Status",
-    purgeOptions: {
-      displayCount: 10000
-    },
-    elevationInfo: {
-      mode: "on-the-ground"
-    },
-    definitionExpression: "STATUS in ('Offen', 'In Vorbereitung', 'Geschlossen')",
-    renderer: new UniqueValueRenderer({
-      field: "STATUS",
-      defaultSymbol: defaultSlopeSymbol,
-      uniqueValueInfos: [
-        {
-          value: "Offen",
-          symbol: openSlopeSymbol
-        },
-        {
-          value: "In Vorbereitung",
-          symbol: preparingSlopeSymbol
-        }
+
+const createSlopeStream = () => new StreamLayer({
+  url,
+  title: "Slopes Status",
+  opacity: 0.4,
+  purgeOptions: {
+    displayCount: 10000
+  },
+  elevationInfo: {
+    mode: "on-the-ground"
+  },
+  definitionExpression: "STATUS in ('Offen', 'In Vorbereitung', 'Geschlossen')",
+  renderer: new UniqueValueRenderer({
+    field: "STATUS",
+    defaultSymbol: new PolygonSymbol3D({
+      symbolLayers: [
+        new FillSymbol3DLayer({
+          material: {
+            color: "#e3e3e3"
+          }
+        })
       ]
-    })
-  });
+    }),
+    uniqueValueInfos: [
+      {
+        value: "Offen",
+        symbol: new PolygonSymbol3D({
+          symbolLayers: [
+            new FillSymbol3DLayer({
+              material: {
+                color: "#4fa0e3"
+              }
+            })
+          ]
+        })
+      },
+      {
+        value: "In Vorbereitung",
+        symbol: new PolygonSymbol3D({
+          symbolLayers: [
+            new FillSymbol3DLayer({
+              material: {
+                color: "#e3ad4f"
+              }
+            })
+          ]
+        })
+      },
+    ]
+  })
+});
 
 export default createSlopeStream;
