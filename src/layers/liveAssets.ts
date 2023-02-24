68f26415d6dbf5298d9e38e3ee0f2d038270f6ba
Tobias Mansfield-Williams <tmansfield-williams@esri.com>
2023-02-24 11:35:44 +0100

    Apply linter

--- src/layers/liveAssets.ts
+++ src/layers/liveAssets.ts
@@ -7,47 +7,48 @@ import LineCallout3D from "@arcgis/core/symbols/callouts/LineCallout3D";
 const url =
   "https://us-iot.arcgis.com/bc1qjuyagnrebxvh/bc1qjuyagnrebxvh/streams/arcgis/rest/services/snowCat_StreamLayer4/StreamServer";
 
-const createAssetsStream = () => new StreamLayer({
-  url,
-  title: "Stream Layer",
-  elevationInfo: {
-    mode: "on-the-ground"
-  },
-  purgeOptions: {
-    displayCount: 10000
-  },
-  labelingInfo: [
-    new LabelClass({
-      labelExpressionInfo: { expression: "return 'Ski Patrol'" },
-      symbol: new LabelSymbol3D({
-        verticalOffset: {
-          screenLength: 20,
-          maxWorldLength: 20,
-          minWorldLength: 5
-        },
-        callout: new LineCallout3D({
-          size: 0.5,
-          color: [0, 0, 0]
-        }),
+const createAssetsStream = () =>
+  new StreamLayer({
+    url,
+    title: "Stream Layer",
+    elevationInfo: {
+      mode: "on-the-ground"
+    },
+    purgeOptions: {
+      displayCount: 10000
+    },
+    labelingInfo: [
+      new LabelClass({
+        labelExpressionInfo: { expression: "return 'Ski Patrol'" },
+        symbol: new LabelSymbol3D({
+          verticalOffset: {
+            screenLength: 20,
+            maxWorldLength: 20,
+            minWorldLength: 5
+          },
+          callout: new LineCallout3D({
+            size: 0.5,
+            color: [0, 0, 0]
+          })
+        })
+      })
+    ],
+    renderer: new SimpleRenderer({
+      symbol: new PointSymbol3D({
+        symbolLayers: [
+          new ObjectSymbol3DLayer({
+            resource: {
+              href: "https://arnofiva.github.io/winter-resort/Bully.glb"
+            },
+            // width: 3.046784222126007,
+            height: 4,
+            heading: 255,
+            tilt: +10
+            // depth: 4.3906859159469604
+          })
+        ]
       })
     })
-  ],
-  renderer: new SimpleRenderer({
-    symbol: new PointSymbol3D({
-      symbolLayers: [
-        new ObjectSymbol3DLayer({
-          resource: {
-            href: "https://arnofiva.github.io/winter-resort/Bully.glb"
-          },
-          // width: 3.046784222126007,
-          height: 4,
-          heading: 255,
-          tilt: +10
-          // depth: 4.3906859159469604
-        })
-      ]
-    })
-  })
-});
+  });
 
 export default createAssetsStream;
--- src/streamServiceMock/events/assetEvents.ts
+++ src/streamServiceMock/events/assetEvents.ts
@@ -1,30 +1,32 @@
-
 import { Polyline, SpatialReference } from "@arcgis/core/geometry";
 import * as geometryEngine from "@arcgis/core/geometry/geometryEngine";
 
-
 const lineTrackId0 = new Polyline({
   spatialReference: SpatialReference.WebMercator,
-  paths:[[
-    [1027346.9501443913,5916224.255862014],
-    [1027567.5299493477,5916280.056826433],
-    [1027783.0920996325,5916316.222072321]
-  ]]
+  paths: [
+    [
+      [1027346.9501443913, 5916224.255862014],
+      [1027567.5299493477, 5916280.056826433],
+      [1027783.0920996325, 5916316.222072321]
+    ]
+  ]
 });
 
 const lineTrackId1 = new Polyline({
   spatialReference: SpatialReference.WebMercator,
-  paths:[[
-    [1026009.8058628943,5920667.152370067],
-    [1025873.5004748334,5920638.377053236],
-    [1025782.5464213139,5920518.475904019],
-    [1025718.6841243431,5920355.685148251],
-    [1025712.4749260369,5920318.2018302595],
-    [1025829.58862274,5920263.6518420605],
-    [1025956.635863282,5920191.491471202],
-    [1026055.7033598219,5920166.048383352]
-  ]]}
-);
+  paths: [
+    [
+      [1026009.8058628943, 5920667.152370067],
+      [1025873.5004748334, 5920638.377053236],
+      [1025782.5464213139, 5920518.475904019],
+      [1025718.6841243431, 5920355.685148251],
+      [1025712.4749260369, 5920318.2018302595],
+      [1025829.58862274, 5920263.6518420605],
+      [1025956.635863282, 5920191.491471202],
+      [1026055.7033598219, 5920166.048383352]
+    ]
+  ]
+});
 
 const densLineTrackId0 = geometryEngine.densify(lineTrackId0, 10.5, "meters") as Polyline;
 
@@ -33,7 +35,7 @@ const track0Messages = densLineTrackId0.paths[0].map((v, idx) => ({
     attributes: {
       track_id: 0,
       x: v[0],
-      y: v[1],
+      y: v[1]
     },
     geometry: {
       x: v[0],
@@ -50,7 +52,7 @@ const track1Messages = track1Dense.paths[0].map((v, idx) => ({
     attributes: {
       track_id: 1,
       x: v[0],
-      y: v[1],
+      y: v[1]
     },
     geometry: {
       x: v[0],
